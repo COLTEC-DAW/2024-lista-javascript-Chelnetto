0c046e1842a685b1fdca85b402c3cd56c6b83df1
Chelnetto <chelnetto@gmail.com>
2024-06-19 11:02:50 -0300

    commit1

---
+++ teste.js
@@ -0,0 +1,6 @@
+var sharps = 9; // colocar input do usuário
+        let result = "";
+        for (let i = 0; i <= sharps; i++) {
+            result +="#";
+                console.log(result);
+        }
